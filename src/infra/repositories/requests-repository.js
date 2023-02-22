4e512f73d43aa9cee4b147f6afd674c42a78bc0c
Luan Martins <luan.cbm@hotmail.com>
2023-02-22 20:51:30 -0300

    fix: att request return fields

--- src/infra/repositories/requests-repository.js
+++ src/infra/repositories/requests-repository.js
@@ -2,6 +2,19 @@ const db = require('../database/models')
 
 exports.getRequetsWaitingStatus = () => {
     return db.solicitacao.findAll({
+        attributes: [
+            ['username', 'author'],
+            ['data_solicitacao', 'date_required'],
+            ['tipo_solicitacao', 'type'],
+            ['nro_protocolo', 'protocol_number'],
+            ['estado', 'state'],
+            ['administrador', 'administrator'],
+            ['voto_admin', 'admin_vote'],
+            ['texto_rejeicao', 'rejection_text'],
+            ['texto_edicao', 'edit_text'],
+            ['estrategia_referente', 'relating_strategy']
+            
+        ],
         where: {
             estado: 0
         }
